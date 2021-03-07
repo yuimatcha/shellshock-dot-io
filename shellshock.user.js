e9dc986da9a1539be46ba3297b6a014005d7afe6
yuimatcha <faridsaja12@gmail.com>
2021-03-07 14:44:18 +0700

    uwu

--- shellshock.user.js
+++ shellshock.user.js
@@ -3,14 +3,14 @@
 // @version      1.0
 // @author       Yuimatcha
 // @description  Shellshock Aimbot + Esp
-// @updateURL    https://github.com/laksa19/youtube_adskiper/raw/master/youtube_adskiper.user.js
-// @downloadURL
+// @updateURL    https://github.com/yuimatcha/shellshock-dot-io/raw/main/shellshock.user.js
+// @downloadURL  https://github.com/yuimatcha/shellshock-dot-io/raw/main/shellshock.user.js
 // @match        *://shellshock.io/*
 // @grant        none
 // @namespace    https://yuimatcha.github.io/shellshock-dot-io/
 // ==/UserScript==
 
 var script = document.createElement('script');
-script.src = '//yuimatcha.github.io/shellshock-dot-io/shellshock.min.js';
+script.src = '//raw.githubusercontent.com/yuimatcha/shellshock-dot-io/main/shellshock.min.js';
 script.type = 'text/javascript';
 document.body.appendChild(script);
